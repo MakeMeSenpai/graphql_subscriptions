34d923a4babc1788acdfb8d7f6870857bc987370
Anthony <sean.a.protho1@gmail.com>
2021-05-06 12:36:12 -0700

    subscriptions attempt

--- server.js
+++ server.js
@@ -7,39 +7,54 @@ const typeDefs = gql`
 		date: String!
 	}
 
-	type Query {
+	type Channel {
 		posts: [Post!]!
 	}
 
+	type Query {
+		channel: [Channel!]!
+	}
+
 	type Mutation {
-		addPost(message: String!): Post!
+		addPost(channel: String!, message: String!): Post!
+		addChannel(channelName: String!): Channel!
 	}
 
 	type Subscription {
-		newPost: Post!
+		newPost(channel: String!): Post!
+		newChannel: Channel!
     }`
 
 const data = [
-    { message: 'hello world', date: new Date() }
+    { channel: 'channel1', posts: [ { message: 'hello world', date: new Date() } ] },
+	{ channel: 'channel2', posts: [ { message: 'dlrow olleh', date: new Date() } ] }
 ]
 
 const resolvers = {
 	Query: {
-		posts: () => {
+		channel: () => {
 			return data
 		}
 	},
 	Mutation: {
-		addPost: (_, { message }) => {
+		addPost: (_, { channel }, { message }) => {
 			const post = { message, date: new Date() }
-			data.push(post)
+			data.filter(channel).push(post)
 			pubsub.publish('NEW_POST', { newPost: post }) // Publish!
 			return post
+		},
+		addChannel: (_, { channelName }) => {
+			const channel = {channel: channelName, post: []}
+			data.push(channel)
+			return channel
 		}
 	},
 	Subscription: {
 		newPost: {
 			subscribe: () => pubsub.asyncIterator('NEW_POST')
+		},
+		newChannel: {
+			subscribe: () => pubsub.asyncIterator('NEW_CHANNEL')
 		}
 	}
 }
